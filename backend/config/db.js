6a2e3842a7335e5f979bee7d40891227fee869f8
Daksh Prajapati Alian <daksh.prajapati@aliansoftware.com>
2026-03-09 14:49:08 +0530

    Connect databese With neon

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 import {Pool} from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const pool = new Pool({
-    user: "postgres",
-    password: "root",
-    host: "localhost",
-    port: 5432,
-    database: "delivery_webapp"
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 })
 
 export default pool;
